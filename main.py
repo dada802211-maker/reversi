6e88441d2f9f3edb052faca9a6a42af4829a7192
sa <dada802211@gmail.com>
2026-05-05 02:05:24 +0900

    13番麺グループ

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import config
 
 from cui import start as cui
+# from gui import start as gui
+
+# from rev import data, board_group
+# data.board = board_group.get(['win', 'lose', 'skip_e', 'skip_m'][0])    # 盤面の選択
 
 config.init_from_args()
 if config.ui_type == config.TYPE_CUI:
