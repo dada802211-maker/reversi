6f28d7204fd1e34eb2b967f92f7974c368e3bb38
sa <dada802211@gmail.com>
2026-08-27 03:40:40 +0900

    25進行：裏返せるのマスのリスト

--- rev/op.py
+++ rev/op.py
@@ -52,18 +52,17 @@ def get_line(board, start_x, start_y, dir_x, dir_y):
 
 # 裏返せるマスのリストを得る
 def get_reverse(line, player):
-    return []
-#     enemy = 1 - player
+    enemy = 1 - player
 
     # 前提条件
-    # if len(line) < 2: return []         # 2マスない → はさめない
-    # if line[0]['p'] != enemy: return [] # 1マス目が敵でない → はさめない
+    if len(line) < 2: return []         # 2マスない → はさめない
+    if line[0]['p'] != enemy: return [] # 1マス目が敵でない → はさめない
                                         # 1マス目が敵 → 処理を継続する
 
     # マスをたどっていく
-    # res = [line[0]]     # 1マス目を格納
-    # for sq in line[1:]:
-    #     if sq['p'] == enemy: res.append(sq) # 敵マス→リストに追加
-    #     if sq['p'] == data.BLANK: return [] # 空マス→失敗
-    #     if sq['p'] == player: return res;   # 自石→成功 リストを返す
-    # return []   # 末尾まで自石ではさめなかった
+    res = [line[0]]     # 1マス目を格納
+    for sq in line[1:]:
+        if sq['p'] == enemy: res.append(sq) # 敵マス→リストに追加
+        if sq['p'] == data.BLANK: return [] # 空マス→失敗
+        if sq['p'] == player: return res;   # 自石→成功 リストを返す
+    return []   # 末尾まで自石ではさめなかった
