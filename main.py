266f0f84b9d23e15cec269fc4ca3fa007d6c4e25
sa <dada802211@gmail.com>
2026-08-27 16:02:53 +0900

    ウィンドウ初期化

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import config
 from cui import start as cui
-# from gui import start as gui
+from gui import start as gui
 
 # from rev import data, board_group
 # data.board = board_group.get(['win', 'lose', 'skip_e', 'skip_m'][0])    # 盤面の選択
@@ -9,4 +9,4 @@ config.init_from_args()
 if config.ui_type == config.TYPE_CUI:
   cui.loop()    # メインループ
 if config.ui_type == config.TYPE_GUI:
-  print('This is GUI')
+  gui.start()   # 開始
---
+++ xxxDOC/38GUI開始時の処理.md
@@ -0,0 +1,5 @@
+38GUI開始時の処理
+
+gui/start.pyを作る
+
+python -B main.py gui
